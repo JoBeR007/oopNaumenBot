e0f4e2414ed0db0f8fa9c30206e653a559674bcc
JoBeR <kdv200265@gmail.com>
2024-03-31 18:40:59 +0500

    Example usage in main for Arseniy

--- src/main/java/ru/urfu/Main.java
+++ src/main/java/ru/urfu/Main.java
@@ -1,14 +1,30 @@
 package ru.urfu;
 
 import lombok.extern.slf4j.Slf4j;
+import ru.urfu.NyaParser.NyaParser;
+import ru.urfu.RutrackerParser.RutrackerParser;
+import ru.urfu.TorrentFileDownloader.TorrentFileDownloader;
 import ru.urfu.model.Torrent;
+
+import java.util.List;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        log.info("info");
-        log.warn("warn");
-        log.error("error");
+        RutrackerParser rp = RutrackerParser.getInstance();
+        List<Torrent> res = rp.search("dune");
+        for(Torrent  t: res) {
+            System.out.println(t);
+        }
+        TorrentFileDownloader tfd = new TorrentFileDownloader();
+
+        NyaParser np = NyaParser.getInstance();
+        List<Torrent> anime = np.search("naruto");
+        for(Torrent t: anime) {
+            System.out.println(t);
+        }
+        System.out.println(tfd.downloadTorrentFile(res.get(0).getTorrentDownloadLink(), true));
+        System.out.println(tfd.downloadTorrentFile(anime.get(0).getTorrentDownloadLink(), false));
 
     }
 }
